639b5390dceaa5720bed70deaf34b78e08c84c5e
Omar Osman Mahat <74245520+thekingsmakers@users.noreply.github.com>
2024-12-22 12:58:29 +0300

    arrange

---
+++ Geofence/Camera/unblock camera.ps1
@@ -0,0 +1,8 @@
+﻿# Enable the camera explicitly
+$camera = Get-PnpDevice | Where-Object { $_.FriendlyName -like "*Camera*" }
+if ($camera) {
+    $camera | Enable-PnpDevice -Confirm:$false
+    Write-Output "Camera has been enabled."
+} else {
+    Write-Output "Camera device not found."
+}
